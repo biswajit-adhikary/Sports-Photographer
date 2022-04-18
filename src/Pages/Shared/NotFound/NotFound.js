1e9155249baab1db7fc5fa92a4771c42fe3822da
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-04-18 17:32:57 +0600

    404 page added

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Hero from '../Hero/Hero';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>This is not found</h2>
-        </div>
+        <>
+            <Hero></Hero>
+            <div className="not-found">
+                <Container>
+                    <h2>404</h2>
+                    <h3>Page Not Found!</h3>
+                </Container>
+            </div>
+        </>
     );
 };
 
